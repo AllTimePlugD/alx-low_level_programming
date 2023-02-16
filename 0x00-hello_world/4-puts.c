45355aef4af860349b49337208634ac3be4613eb
AllTimePlugD <watuasamuel@gmail.com>
2023-02-16 05:03:12 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
-#!/bin/bash
+!/bin/bash
 #include <stdio.h>
-/**
- * main - Entry point
- * Return: Always o (success)
- */
+
+int main(void)
+{
+    puts("Programming is like building a multilingual puzzle,");
+    return 0;
+}
