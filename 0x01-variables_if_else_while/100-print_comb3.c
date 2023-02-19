1a214f4fc648145ff38cb07a015716ee72057906
AllTimePlugD <watuasamuel@gmail.com>
2023-02-19 14:18:02 -0800

    100print

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,33 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 
 /**
- * main - program that prints all possible different combinations of two digits
- * Numbers must be separated by ,followed by a space
- * The two digits must be different
- * 01 and 10 are considered the same combination of the two digits 0 and 1
- * Return: 0
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int i = 48;
-	int j;
-	int cal = 44;
-	int sp = 32;
-
-	for (; i <= 57; i++)
+int i;
+for (i = 0; i < 100 ; i++)
+{
+	if ((i / 10) < (i % 10))
 	{
-		j = i + 1;
-		for (; j <= 57; j++)
-		{
-			putchar (i);
-			putchar (j);
-			if (i != 56 || (i == 56 && j != 57))
-			{
-				putchar (cal);
-				putchar (sp);
-			}
-		}
+		putchar((i / 10) + '0');
+		putchar((i % 10) + '0');
 	}
-	putchar ('\n');
-	return (0);
+	if (i < 89 && (i / 10) < (i % 10))
+	{
+		putchar(',');
+		putchar(' ');
+	}
+}
+putchar('\n');
+return (0);
 }
