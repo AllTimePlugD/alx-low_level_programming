316351a3a1582c8e5c05822b14175e01799a8e7c
AllTimePlugD <watuasamuel@gmail.com>
2023-02-19 14:01:48 -0800

    8print

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,27 +1,23 @@
-#include<stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
  *
- * Description: print numbers of base16 in lowercase
- *
  * Return: Always 0 (Success)
-*/
-
+ */
 int main(void)
 {
-	int num = 48; /*48; decimal rep of 0*/
-
-	while (num <= 102) /*102; decimal rep of f*/
-	{
-		putchar(num);
-
-		/* after 9 we jump till 96; `*/
-		if (num == 57)
-			num += 39;
-		++num;
-	}
-	putchar('\n');
-
-	return (0);
+int c;
+for (c = '0'; c <= '9'; c++)
+{
+putchar(c);
+}
+for (c = 'a'; c <= 'f'; c++)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
 }
