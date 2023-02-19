b01045e5d262d2859290e020165a50403b084584
AllTimePlugD <watuasamuel@gmail.com>
2023-02-19 13:46:42 -0800

    sssam

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,28 +1,35 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    int i, j;
+int i = 0;
+int a;
 
-    for (i = 0; i < 100; i++)
-    {
-        for (j = i + 1; j < 100; j++)
-        {
-            putchar(i / 10 + '0');
-            putchar(i % 10 + '0');
-            putchar(' ');
-            putchar(j / 10 + '0');
-            putchar(j % 10 + '0');
-
-            if (i != 98 || j != 99)
-            {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
-    putchar('\n');
-
-    return 0;
+for (a = 1; a < 100 ; a++)
+{
+	putchar((i / 10) + '0');
+	putchar((i % 10) + '0');
+	putchar(' ');
+	putchar((a / 10) + '0');
+	putchar((a % 10) + '0');
+	if (i < 98)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	if (a == 99)
+	{
+		i++;
+		a = i;
+	}
 }
-
+putchar('\n');
+return (0);
+}
