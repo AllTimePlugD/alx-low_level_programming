5df0a3eb142b3356aa793c69c8cac3cf86826e7a
AllTimePlugD <watuasamuel@gmail.com>
2023-02-19 12:42:11 -0800

    soi

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,23 +1,15 @@
-#include <stdio.h>
-
-/**
- * main - Entry point
- *
- * Description: print all aplhabet letters
- *
- * Return: Always 0 (Success)
-*/
-
 int main(void)
 {
-	char ch = 'a';
+    char letter = 'a';
 
-	while (ch <= 'z')
-	{
-		putchar(ch);
-		++ch;
-	}
-	putchar('\n');
+    while (letter <= 'z')
+    {
+        putchar(letter);
+        letter++;
+    }
 
-	return (0);
+    putchar('\n');
+
+    return 0;
 }
+
