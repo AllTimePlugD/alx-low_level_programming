d9f06e694918af5d6bfe7086735ee8c949aca1d1
AllTimePlugD <watuasamuel@gmail.com>
2023-02-20 11:50:25 -0800

    5printme

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,22 +1,18 @@
-#include<stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
  *
- * Description: print all single digit numbers
- *
  * Return: Always 0 (Success)
-*/
-
+ */
 int main(void)
 {
-    int i;
-
-    for (i = 0; i < 10; i++)
-    {
-        putchar(i + '0');
-    }
-    putchar('\n');
-
-    return 0;
+int c;
+for (c = 0; c <= 9; c++)
+{
+	printf("%d", c);
+}
+putchar('\n');
+return (0);
 }
