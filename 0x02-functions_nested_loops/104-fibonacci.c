1064bd82554309d50b2e4f1731ca5c777d1d9583
AllTimePlugD <watuasamuel@gmail.com>
2023-02-21 13:26:53 -0800

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -2,7 +2,7 @@
 /**
  * Main - Entry point of program
  * Finds first 100 fibonacci numbers
- *prints starting with 1 and 2,followed by new line 
+ *prints starting with 1 and 2,followed by newline 
  * Return: Always 0 (success)
  */
 int main(void)
@@ -10,7 +10,7 @@ int main(void)
 int i;
 unsigned int a = 1, b = 2, c;
 printf("%u, %u", a, b);
-for (i = 3; i <= 98; i++) 
+for (i = 3; i <= 98; i++)
 {
 c = a + b;
 printf(", %u", c);
