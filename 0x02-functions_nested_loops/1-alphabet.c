66b99f88cddf81ae8a7122f59c08a63a90304eb1
AllTimePlugD <watuasamuel@gmail.com>
2023-02-21 11:17:43 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - entry point
+ * Description - prints all alphabet, in lowercase, followed by a new line
+ * Return 0
+ */
+void print_alphabet(void)
+{
+char chr;
+chr = 'a';
+while (chr <= 'z')
+{
+_putchar(chr);
+chr++;
+}
+_putchar('\n');
+}
