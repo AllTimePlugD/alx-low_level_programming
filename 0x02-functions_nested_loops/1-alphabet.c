3a33b284d7919719e8aeca960a736a28c89948b2
AllTimePlugD <watuasamuel@gmail.com>
2023-02-21 02:51:45 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,13 @@
 #include "main.h"
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main : Entry point
+ * Return: Always 0
  */
-void print_alphabet(void)
+
+int main(void)
 {
-	char c;
-	for (c = 'a'; c <= 'z'; c++)
-	{
-		_putchar(c);
-	}
-_putchar('\n');
+	   print_alpahabet();
+	   return (0)
+
 }
