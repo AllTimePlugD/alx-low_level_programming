fcdd7687d98ee0237b74d9cbc665302cc60828e6
AllTimePlugD <watuasamuel@gmail.com>
2023-02-21 11:10:52 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,4 @@
-int_putchar(char c);
+int _putchar(char c);
 
 void print_alphabet(void);
 
@@ -16,10 +16,12 @@ int print_last_digit(int);
 
 void jack_bauer(void);
 
-void times_table(void)
+void times_table(void);
 
 int add(int, int);
 
 void print_to_98(int n);
 
 void print_times_table(int n);
+
+
