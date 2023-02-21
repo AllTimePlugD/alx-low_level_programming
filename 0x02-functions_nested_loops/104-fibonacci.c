b5a2c50bc35f438b7092f7b3754dade468bc25cd
AllTimePlugD <watuasamuel@gmail.com>
2023-02-21 13:06:20 -0800

    104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * Main - Entry point
+ * Finds and prints first 100 fibonacci numbers, starting with 1 and 2, followed by new line
+ * Return: Always 0
+ */
+int main(void)
+{
+int i;
+unsigned int a = 1, b = 2, c;
+printf("%u, %u", a, b);  // print first two numbers
+for (i = 3; i <= 98; i++) {
+c = a + b;
+printf(", %u", c);
+a = b;
+b = c;
+}
+printf("\n");  // print newline character
+return 0;
+}
