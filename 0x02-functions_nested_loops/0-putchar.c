2912202dab50ab40ad628fb05bda3de7af6a93d7
AllTimePlugD <watuasamuel@gmail.com>
2023-02-21 10:33:46 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,20 @@
 #include "main.h"
+
 /**
- * main - Entry Block
-  *Description: prints _putchar followed by a new line
- * Return: 0
+ * main - Entry point
+ *Description: prints _putchar followed by a new line
+ * Return
  */
 int main(void)
 {
-char c[8] = "_putchar";
-int i = 0;
-while (i < 8)
-{
-_putchar(c[i]);
-i++;
-}
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 _putchar('\n');
 return (0);
 }
