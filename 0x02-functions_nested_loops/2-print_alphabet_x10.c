3aae8058c6d123b73529467de625de2c025c7cc7
AllTimePlugD <watuasamuel@gmail.com>
2023-02-21 10:47:03 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
- * followed by a new line
+ * print_alphabet_x10 - prints the alphabet ten times
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 void print_alphabet_x10(void)
 {
-char chr;
-int i = 0;
-while (i <= 9)
+char c;
+int n = 1;
+for (n = 1; n <= 10; n++)
 {
-for (chr = 'a'; chr <= 'z'; chr++)
+for (c = 'a'; c <= 'z'; c++)
 {
-_putchar(chr);
+_putchar(c);
 }
 _putchar('\n');
-i++;
 }
 }
